4dab4cd24f97aa7ac05cab2384bc99cec7f466db
Anirudh Saligram <anirudh.saligram@gmail.com>
2018-02-22 10:16:14 -0800

    readEmail - initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const imaps = require('imap-simple');
+let connection;
+
+function readEmail() {
+    const config = {
+        imap: {
+            user: 'myday.sap@gmail.com',
+            password: 'myday@sap',
+            host: 'imap.gmail.com',
+            port: 993,
+            tls: true,
+            authTimeout: 3000
+        }
+    };
+
+    return imaps.connect(config)
+        .then((connected) => {
+            console.log('connection success!!!');
+            connection = connected;
+            return connection.openBox('INBOX');
+        })
+        .then(() => {
+            const searchCriteria = ['UNSEEN'];
+            const fetchOptions = {
+                bodies: ['HEADER', 'TEXT'],
+                markSeen: false
+            };
+            return connection.search(searchCriteria, fetchOptions);
+        })
+        .then((results) => {
+            console.log(results);
+        })
+
+
+}
+
+try {
+    return readEmail();
+}
+catch (e) {
+    console.log(e);
+}
